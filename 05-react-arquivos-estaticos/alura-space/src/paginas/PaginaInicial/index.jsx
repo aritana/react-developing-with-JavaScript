0552c84e558ceffac5aa0fe470a085f65eca4e18
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2023-01-05 23:37:18 -0300

    projetos e avancando nos componentes

---
+++ 05-react-arquivos-estaticos/alura-space/src/paginas/PaginaInicial/index.jsx
@@ -0,0 +1,21 @@
+import Cabecalho from "../../components/Cabecalho";
+import Menu from "../../components/Menu";
+import banner from "./banner.png"
+import styles from "./PaginaInicial.module.scss"
+
+export default function PaginaInicial() {
+    return (
+        <>
+            <Cabecalho />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="A imagem da terra vista do espaço"></img>
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
